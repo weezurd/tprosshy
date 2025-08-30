f21ad6a48ac95c7f0c7278ced52c7c5cbbae4ffd
weezurd <dungphamdang99@gmail.com>
2025-08-30 23:23:09 +0700

    Utilize socket2 instead of libc

--- src/methods.rs
+++ src/methods.rs
@@ -1,47 +1,27 @@
 mod nft;
 
-use libc::{SOL_IP, c_int, sockaddr_in, socklen_t};
+use socket2;
 use std::error::Error;
-use std::fmt::Write as FmtWrite;
-use std::io;
+use std::net::SocketAddrV4;
 
-const SO_ORIGINAL_DST: c_int = 80;
-fn get_original_dst(fd: i32) -> Result<String, Box<dyn Error>> {
-    let mut addr: sockaddr_in = unsafe { std::mem::zeroed() };
-    let mut len = std::mem::size_of::<sockaddr_in>() as socklen_t;
-
-    let ret = unsafe {
-        libc::getsockopt(
-            fd,
-            SOL_IP,
-            SO_ORIGINAL_DST,
-            &mut addr as *mut _ as *mut _,
-            &mut len,
-        )
-    };
-
-    if ret != 0 {
-        return Err(Box::new(io::Error::last_os_error()));
-    }
-
-    let ip_bytes = addr.sin_addr.s_addr.to_ne_bytes(); // In native-endian
-    let ip = format!(
-        "{}.{}.{}.{}",
-        ip_bytes[0], ip_bytes[1], ip_bytes[2], ip_bytes[3]
-    );
-
-    let port = u16::from_be(addr.sin_port); // Port is big-endian
-
-    let mut output = String::new();
-    write!(&mut output, "{}:{}", ip, port)?;
-
-    Ok(output)
+fn get_original_dst(sock_ref: socket2::SockRef) -> Result<SocketAddrV4, Box<dyn Error>> {
+    let dst_addr_v4 = sock_ref
+        .original_dst_v4()
+        .expect("Failed to get orginal destination")
+        .as_socket_ipv4()
+        .expect("Failed to convert original destination to ipv4");
+    return Ok(dst_addr_v4);
 }
 
 pub trait BaseMethod {
-    fn setup_fw(&self, allow_ips: &str, local_server_port: u16) -> Result<(), Box<dyn Error>>;
+    fn setup_fw(
+        &self,
+        allow_ips: &str,
+        ssh_host_ip: &str,
+        local_server_port: u16,
+    ) -> Result<(), Box<dyn Error>>;
     fn restore_fw(&self) -> Result<(), Box<dyn Error>>;
-    fn get_original_dst(&self, fd: i32) -> Result<String, Box<dyn Error>>;
+    fn get_original_dst(&self, sock_ref: socket2::SockRef) -> Result<SocketAddrV4, Box<dyn Error>>;
 }
 
 pub fn get_available_method() -> Box<dyn BaseMethod + Send + Sync> {
