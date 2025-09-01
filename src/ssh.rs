5fd4addfef89615dbc922d620a4ddaf6a201e01d
weezurd <dungphamdang99@gmail.com>
2025-09-01 15:43:43 +0700

    Support ssh w/o command

--- src/ssh.rs
+++ src/ssh.rs
@@ -2,29 +2,42 @@ use std::{error::Error, process::Stdio};
 
 use tokio::process::{Child, Command};
 
-pub fn ssh(user: &str, host: &str, port: u16, identity_file: &str, remote_command: &str) -> Child {
-    Command::new("ssh")
-        .args([
-            "-p",
-            &port.to_string(),
-            "-o",
-            "StrictHostKeyChecking=no",
-            "-o",
-            "UserKnownHostsFile=/dev/null",
-            "-o",
-            "LogLevel=ERROR",
-            "-o",
-            "ControlPath=~/.ssh/cm-%r@%h:%p",
-            "-o",
-            "ControlMaster=auto",
-            "-o",
-            "ControlPersist=10m",
-            "-i",
-            identity_file,
-            &format!("{}@{}", user, host),
-            remote_command,
-        ])
-        .stdin(Stdio::piped())
+pub fn ssh(
+    user: &str,
+    host: &str,
+    port: u16,
+    identity_file: &str,
+    remote_command: Option<&str>,
+) -> Child {
+    let mut cmd = Command::new("ssh");
+    cmd.arg("-p");
+    cmd.arg(&port.to_string());
+    cmd.arg("-o");
+    cmd.arg("StrictHostKeyChecking=no");
+    cmd.arg("-o");
+    cmd.arg("UserKnownHostsFile=/dev/null");
+    cmd.arg("-o");
+    cmd.arg("LogLevel=ERROR");
+    cmd.arg("-o");
+    cmd.arg("ControlPath=~/.ssh/cm-%r@%h:%p");
+    cmd.arg("-o");
+    cmd.arg("ControlMaster=auto");
+    cmd.arg("-o");
+    cmd.arg("ControlPersist=10m");
+    cmd.arg("-i");
+    cmd.arg(identity_file);
+    match remote_command {
+        Some(x) => {
+            cmd.arg(&format!("{}@{}", user, host));
+            cmd.arg(x);
+        }
+        None => {
+            cmd.arg("-N");
+            cmd.arg(&format!("{}@{}", user, host));
+        }
+    };
+
+    cmd.stdin(Stdio::piped())
         .stdout(Stdio::piped())
         .spawn()
         .expect("Failed to ssh")
