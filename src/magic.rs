fe247959b33cd3cd5f648949c5bef98f5d198fdd
weezurd <dungphamdang99@gmail.com>
2025-09-30 18:03:49 +0700

    Add new const

--- src/magic.rs
+++ src/magic.rs
@@ -1,3 +1,5 @@
 pub const DATAGRAM_MAXSIZE: usize = 65536;
+pub const BUFSIZE: usize = 65536;
+pub const MAX_CHANNEL: u32 = 65536;
 pub const LOCAL_TCP_PORT: u16 = 8080;
 pub const LOCAL_UDP_PORT: u16 = 8081;
