bec45186f84a4c2037146abacc4488c0ce362f45
weezurd <dungphamdang99@gmail.com>
2025-11-18 21:22:09 +0700

    Remove unused code

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,4 @@
 use chrono::Local;
-use core::task::Context;
-use core::task::Poll;
 use env_logger;
 use env_logger::Env;
 use log::warn;
@@ -10,12 +8,9 @@ use std::fs;
 use std::fs::OpenOptions;
 use std::io::Write;
 use std::net::SocketAddrV4;
-use std::pin::Pin;
-use tokio::io::{self, AsyncRead, AsyncWrite, ReadBuf, Stdin, Stdout};
-use tokio::process::{ChildStdin, ChildStdout};
 
 pub fn init_logger(file_path: Option<String>) {
-    let env = Env::default().filter_or("RUST_LOG", "debug");
+    let env = Env::default().filter_or("RUST_LOG", "info");
     let mut logger = env_logger::Builder::from_env(env);
     logger.format(|buf, record| {
         writeln!(
@@ -46,70 +41,6 @@ pub fn init_logger(file_path: Option<String>) {
     }
 
     logger.init();
-}
-
-pub enum Tx {
-    Child(ChildStdin),
-    Std(Stdout),
-}
-
-pub enum Rx {
-    Child(ChildStdout),
-    Std(Stdin),
-}
-
-pub struct IOWrapper {
-    pub tx: Tx,
-    pub rx: Rx,
-}
-
-impl IOWrapper {
-    pub fn default() -> Self {
-        return IOWrapper {
-            tx: Tx::Std(tokio::io::stdout()),
-            rx: Rx::Std(tokio::io::stdin()),
-        };
-    }
-}
-
-impl AsyncRead for IOWrapper {
-    fn poll_read(
-        self: Pin<&mut Self>,
-        cx: &mut Context<'_>,
-        buf: &mut ReadBuf<'_>,
-    ) -> Poll<io::Result<()>> {
-        match &mut self.get_mut().rx {
-            Rx::Child(x) => Pin::new(x).poll_read(cx, buf),
-            Rx::Std(x) => Pin::new(x).poll_read(cx, buf),
-        }
-    }
-}
-
-impl AsyncWrite for IOWrapper {
-    fn poll_write(
-        self: Pin<&mut Self>,
-        cx: &mut Context<'_>,
-        data: &[u8],
-    ) -> Poll<io::Result<usize>> {
-        match &mut self.get_mut().tx {
-            Tx::Child(x) => Pin::new(x).poll_write(cx, data),
-            Tx::Std(x) => Pin::new(x).poll_write(cx, data),
-        }
-    }
-
-    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
-        match &mut self.get_mut().tx {
-            Tx::Child(x) => Pin::new(x).poll_flush(cx),
-            Tx::Std(x) => Pin::new(x).poll_flush(cx),
-        }
-    }
-
-    fn poll_shutdown(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
-        match &mut self.get_mut().tx {
-            Tx::Child(x) => Pin::new(x).poll_shutdown(cx),
-            Tx::Std(x) => Pin::new(x).poll_shutdown(cx),
-        }
-    }
 }
 
 pub fn get_system_resolvers() -> Vec<SocketAddrV4> {
